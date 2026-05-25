6fb3cca809f56f63a5966211d55d63a4d7363221
evalew0520 <eva.lew.tl@gmail.com>
2026-05-25 12:36:18 +0800

    chore: remove root config files and clean up

--- user-app/.next/dev/server/middleware-build-manifest.js
+++ user-app/.next/dev/server/middleware-build-manifest.js
@@ -3,24 +3,11 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/0edr_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [
     "static/development/_buildManifest.js",
     "static/development/_ssgManifest.js",
     "static/development/_clientMiddlewareManifest.js"
   ],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0yca9zn._.js",
-    "static/chunks/0edr_next_dist_compiled_next-devtools_index_0zlnzyq.js",
-    "static/chunks/0edr_next_dist_compiled_react-dom_0lbb4wv._.js",
-    "static/chunks/0edr_next_dist_compiled_react-server-dom-turbopack_0o-8lax._.js",
-    "static/chunks/0edr_next_dist_compiled_0gw88im._.js",
-    "static/chunks/0edr_next_dist_client_0_p39j~._.js",
-    "static/chunks/0edr_next_dist_084xone._.js",
-    "static/chunks/0edr_@swc_helpers_cjs_09aagz2._.js",
-    "static/chunks/user-app_0rqeker._.js",
-    "static/chunks/turbopack-user-app_03zp3-7._.js"
-  ]
+  "rootMainFiles": []
 };
